c591ccacef743d164988ce04fc98820c7a333154
Ssopaa <tpgus1231234@naver.com>
2025-01-23 15:12:20 +0900

    구조 재설계 작업
     - 송수민씨 환영합니다 ^^

---
+++ level 14/Sehyun/ex1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b;
+    scanf("%d",&a);
+    int A[a];
+    for(int i=0; i<a; i++){
+        scanf("%d",&A[i]);
+    }
+    scanf("%d",&b);
+    int B[b], C[b];
+    for(int i=0; i<b; i++){
+        scanf("%d",&B[i]);
+        for(int j=0; j<a; j++){
+            if(B[i]==A[j]){
+                C[i]=1;
+                break;
+            }
+            else{
+                C[i]=0;
+            }
+        }
+        printf("hihihihi");
+        printf("%d ",C[i]);
+    }
+
+    return 0;
+}
